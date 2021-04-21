ba5630ed1bf0ff198e91ad2f90c6fb44d3435719
PRINCEKK121 <prince.karikari@amalitech.org>
2021-04-21 17:00:38 +0000

    commiting

--- dist/js/highScores.js
+++
@@ -1,4 +0,0 @@
-const highScoresList = document.querySelector('#highScoresList');
-const highScores = JSON.parse(localStorage.getItem('highScoresData')) || [];
-
-highScoresList.innerHTML = highScores.map(score =>`<li class="high-score">${score.name} - ${score.score}</li>`).join('');
